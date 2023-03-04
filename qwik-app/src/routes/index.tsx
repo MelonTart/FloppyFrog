535a796ac8d9bf42bc2119545cdce9f325e33c5b
MelonTart <colemarquard@gmail.com>
2023-03-03 16:15:50 -0800

    First Commit

---
+++ qwik-app/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+        Hello World Floppy Frog!
+     </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Floppy Frog',
+  meta: [
+    {
+      name: 'Floppy Frog',
+      content: 'Floppy Frog, Can You Survive?',
+    },
+  ],
+};
